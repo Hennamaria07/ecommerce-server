eaf95804a8fb6bba7400aad0737be7ffeb3b73e6
Henna Maria <hennamaria2001@gmail.com>
2024-05-21 10:45:52 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/user.routes.js";
+import productRoute from "./routes/product.routes.js";
+import categoryRoute from "./routes/category.routes.js";
+import orderRoute from "./routes/order.routes.js";
+
+const app = new express();
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}))
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(cookieParser());
+
+// routes
+app.use("api/v1/user", userRoute);
+app.use("api/v1/product", productRoute);
+app.use("api/v1/category", categoryRoute);
+app.use("api/v1/order", orderRoute);
+
+export default app
