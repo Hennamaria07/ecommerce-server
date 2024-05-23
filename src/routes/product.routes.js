b23ffd98a98b9b2f6b79984456977ffeb4804e6c
Henna Maria <hennamaria2001@gmail.com>
2024-05-23 15:14:56 +0530

    routes for products were added

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,30 @@
 import { Router } from "express";
+import { authorizedAdminOrSeller, authorizedSeller, verifyUser } from "../middlewares/verifyjwt.middleware.js";
+import upload from "../middlewares/multer.middleware.js"
+import { CreateProduct, DeleteProductById, FetchNewProduct, FetchProducts, FetchTopProduct, ProductById, ProductReview, ProductsBySeller, UpdateProductById } from "../controllers/product.controller.js";
 
 const router = new Router();
+
+router.route("/create")
+.post(verifyUser, authorizedAdminOrSeller, upload.single("image"), CreateProduct);
+
+router.route("/:id")
+.get(ProductById)
+.put(verifyUser, authorizedAdminOrSeller,upload.single("image"), UpdateProductById)
+.delete(verifyUser, authorizedAdminOrSeller, DeleteProductById)
+
+router.route("/fetch/products")
+.post(FetchProducts);
+
+router.route("/seller/products")
+.get(verifyUser, authorizedSeller, ProductsBySeller);
+
+router.route("/review/:id")
+.post(verifyUser, ProductReview);
+
+router.route("/top/products")
+.get(FetchTopProduct);
+router.route("/new/products")
+.get(FetchNewProduct);
 
 export default router
