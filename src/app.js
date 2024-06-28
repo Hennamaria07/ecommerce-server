7d59681fab7870c00fa2af6aa9301416f2b16e95
Henna Maria <hennamaria2001@gmail.com>
2024-06-28 09:51:25 +0530

    cors option updated

--- src/app.js
+++ src/app.js
@@ -9,11 +9,13 @@ import orderRoute from "./routes/order.routes.js";
 const app = new express();
 
 app.use(cors({
-    origin: true,
-    credentials: true
+    origin: process.env.FRONTEND_HOST,
+    credentials: true,
+    methods: ["GET", "HEAD", "OPTIONS", "POST", "DELETE", "PUT", "PATCH"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
 // routes
