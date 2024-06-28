86ed222f6e1e7fec019a934ed28d0e5919a7f15e
Henna Maria <hennamaria2001@gmail.com>
2024-06-28 11:33:32 +0530

    testing path addded

--- src/app.js
+++ src/app.js
@@ -9,11 +9,12 @@ import orderRoute from "./routes/order.routes.js";
 const app = new express();
 
 app.use(cors({
-    origin:'*',
+    origin: '*',
     credentials: true,
     methods: ["GET", "HEAD", "OPTIONS", "POST", "DELETE", "PUT", "PATCH"],
     allowedHeaders: ['Content-Type', 'Authorization'],
-}))
+  }));
+  
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
